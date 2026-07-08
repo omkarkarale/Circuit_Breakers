cc5995051c10086538a82679cc59580a7d6df0ce
bottlecoder-raj <hazrarajnarayan55@gmail.com>
2026-07-08 12:46:32 +0530

    Stepper

--- code/Medicine Dispenser/Stepper.ino
+++ code/Medicine Dispenser/Stepper.ino
@@ -4,8 +4,8 @@
 const int stepsPerRevolution = 2048; // 28BYJ-48 full revolution
 
 // IN1, IN3, IN2, IN4 pins on ULN2003
-Stepper myStepper1(stepsPerRevolution, 26,28,27,29);
-Stepper myStepper2(stepsPerRevolution, 30,32,31,33);
+Stepper myStepper1(stepsPerRevolution, 24,26,25,27);
+Stepper myStepper2(stepsPerRevolution, 44,46,45,47);
 Stepper myStepper3 (stepsPerRevolution, 34,36,35,37);
 
 void setup() {
@@ -18,8 +18,11 @@ void setup() {
 void loop() {
   Serial.println("Clockwise");
   myStepper1.step(stepsPerRevolution); // One full revolution clockwise
+  Serial.println("Clockwise1");
   myStepper2.step(stepsPerRevolution); // One full revolution clockwise
+  Serial.println("Clockwise2");
   myStepper3.step(stepsPerRevolution); // One full revolution clockwise
+  Serial.println("Clockwise3");
   delay(1000);
 
   Serial.println("Counterclockwise");
